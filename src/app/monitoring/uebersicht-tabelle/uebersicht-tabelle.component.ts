b2613a9c36f07a971cad772ff6c1e9ebf210a7ef
jens <info@umweltsoft.de>
2024-01-07 20:22:09 +0100

    20240107

--- src/app/monitoring/uebersicht-tabelle/uebersicht-tabelle.component.ts
+++ src/app/monitoring/uebersicht-tabelle/uebersicht-tabelle.component.ts
@@ -17,5 +17,20 @@ export class UebersichtTabelleComponent {
 
 	dataSource=this.wkUebersicht; 
 
-
+  getColor(OZK) {
+    switch (OZK) {
+      case '1':
+        return 'rgb(0, 158, 224)';
+      case '2':
+        return 'rgb(0, 144, 54)';
+        case '3':
+          return 'rgb(255, 255, 0)';
+          case '4':
+        return 'rgb(255, 153, 0)';
+        case '5':
+        return 'rgb(226, 0, 26)';
+        default:
+          return 'withe';
+    }
+  }
 }
