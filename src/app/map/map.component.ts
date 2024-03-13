23fc67d2db5055c9f09e932e5d086b02e7742888
jens <info@umweltsoft.de>
2024-03-13 23:06:13 +0100

    20240313

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,6 +7,7 @@ import TileWMS from 'ol/source/TileWMS.js';
 
 import WMTSCapabilities from 'ol/format/WMTSCapabilities.js';
 import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS';
+import { last } from 'rxjs';
 
 
 @Component({
@@ -15,12 +16,95 @@ import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  // map:Map;
+  map: Map;
+
+
   options:String;
+  layer:string='ne:view_geo_wk_oezk_bp3';
+
+
+  view_geo_lw_oezk_bp2 =   new TileLayer({
+    source:new TileWMS({
+      url:'http://localhost:8080/geoserver/WK/wms',
+      params: {'LAYERS': 'WK:view_geo_lw_oezk_bp2', 'TILED': true},
+      serverType:'geoserver',
+      transition:0,
+  
+    })
+  });
+  view_geo_lw_oezk_bp3 =   new TileLayer({
+    source:new TileWMS({
+      url:'http://localhost:8080/geoserver/WK/wms',
+      params: {'LAYERS': 'WK:view_geo_lw_oezk_bp3', 'TILED': true},
+      serverType:'geoserver',
+      transition:0,
+  
+    })
+  });
+  view_geo_lw_oezk_bp1 =   new TileLayer({
+    source:new TileWMS({
+      url:'http://localhost:8080/geoserver/WK/wms',
+      params: {'LAYERS': 'WK:view_geo_lw_oezk_bp1', 'TILED': true},
+      serverType:'geoserver',
+      transition:0,
+  
+    })
+  });
+view_geo_wk_oezk_bp2 =   new TileLayer({
+  source:new TileWMS({
+    url:'http://localhost:8080/geoserver/ne/wms',
+    params: {'LAYERS': 'ne:view_geo_wk_oezk_bp2', 'TILED': true},
+    serverType:'geoserver',
+    transition:0,
+
+  })
+});
+view_geo_wk_oezk_bp3 =   new TileLayer({
+  source:new TileWMS({
+    url:'http://localhost:8080/geoserver/ne/wms',
+    params: {'LAYERS': 'ne:view_geo_wk_oezk_bp3', 'TILED': true},
+    serverType:'geoserver',
+    transition:0,
+
+  })
+});
+view_geo_wk_oezk_bp1 =   new TileLayer({
+  source:new TileWMS({
+    url:'http://localhost:8080/geoserver/ne/wms',
+    params: {'LAYERS': 'ne:view_geo_wk_oezk_bp1', 'TILED': true},
+    serverType:'geoserver',
+    transition:0,
+
+  })
+});
+startbp1(){
+  this.map.removeLayer( this.view_geo_wk_oezk_bp3)
+  this.map.removeLayer( this.view_geo_wk_oezk_bp2)
+  this.map.addLayer(this.view_geo_wk_oezk_bp1); 
+  this.map.removeLayer( this.view_geo_lw_oezk_bp3)
+  this.map.removeLayer( this.view_geo_lw_oezk_bp2)
+  this.map.addLayer(this.view_geo_lw_oezk_bp1); 
+}
+startbp2(){
+  this.map.removeLayer( this.view_geo_wk_oezk_bp3);
+  this.map.removeLayer( this.view_geo_wk_oezk_bp1);
+  this.map.addLayer(this.view_geo_wk_oezk_bp2);
+  this.map.removeLayer( this.view_geo_lw_oezk_bp3);
+  this.map.removeLayer( this.view_geo_lw_oezk_bp1);
+  this.map.addLayer(this.view_geo_lw_oezk_bp2);
+}
+startbp3(){
+  this.map.removeLayer( this.view_geo_wk_oezk_bp2)
+  this.map.removeLayer( this.view_geo_wk_oezk_bp1)
+  this.map.addLayer(this.view_geo_wk_oezk_bp3);
+  this.map.removeLayer( this.view_geo_lw_oezk_bp2)
+  this.map.removeLayer( this.view_geo_lw_oezk_bp1)
+  this.map.addLayer(this.view_geo_lw_oezk_bp3);
+}
   ngOnInit(): void {
     const parser = new WMTSCapabilities();
     let options;
-    let map;
+   
     
     fetch('https://sgx.geodatenzentrum.de/wmts_basemapde/1.0.0/?SERVICE=WMTS&REQUEST=GetCapabilities')
     .then(function (response) {
@@ -50,34 +134,34 @@ export class MapComponent implements OnInit {
     
 
 
-    map = new Map({
+    this.map = new Map({
       layers: [
         new TileLayer({
           source: new OSM(),
           opacity: 0.7,
         }),
-        new TileLayer({
-          source:new TileWMS({
-            url:'http://localhost:8080/geoserver/ne/wms',
-            params: {'LAYERS': 'ne:rwbody', 'TILED': true},
-            serverType:'geoserver',
-            transition:0,
+        // new TileLayer({
+        //   source:new TileWMS({
+        //     url:'http://localhost:8080/geoserver/ne/wms',
+        //     params: {'LAYERS': this.layer, 'TILED': true},
+        //     serverType:'geoserver',
+        //     transition:0,
 
-          })
-
-
-        }),
-        new TileLayer({
-          source:new TileWMS({
-            url:'http://localhost:8080/geoserver/WK/wms',
-            params: {'LAYERS': 'WK:lwbody', 'TILED': true},
-            serverType:'geoserver',
-            transition:0,
-
-          })
+        //   })
 
 
-        }),
+        // }),
+        // new TileLayer({
+        //   source:new TileWMS({
+        //     url:'http://localhost:8080/geoserver/WK/wms',
+        //     params: {'LAYERS': 'WK:lwbody', 'TILED': true},
+        //     serverType:'geoserver',
+        //     transition:0,
+
+        //   })
+
+
+        // }),
         // new TileLayer({
         //   opacity: 1,
         //   source: new WMTS(options),
